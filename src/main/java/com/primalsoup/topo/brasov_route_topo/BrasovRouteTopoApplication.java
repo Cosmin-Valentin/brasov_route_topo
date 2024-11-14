bfed66bf1433e6056a71420e229b65505764e38f
Cosmin-Valentin <cosminv.nicola@gmail.com>
2024-11-14 09:01:34 +0200

    Initial commit

---
+++ src/main/java/com/primalsoup/topo/brasov_route_topo/BrasovRouteTopoApplication.java
@@ -0,0 +1,13 @@
+package com.primalsoup.topo.brasov_route_topo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasovRouteTopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasovRouteTopoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primalsoup/topo/brasov_route_topo/BrasovRouteTopoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primalsoup.topo.brasov_route_topo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasovRouteTopoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
