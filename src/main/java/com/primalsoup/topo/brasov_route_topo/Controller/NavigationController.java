9859f881a1d5039416d89cbd2b542864ad3248df
Cosmin-Valentin <cosminv.nicola@gmail.com>
2024-12-09 17:26:41 +0200

    Dummy user login wip

--- src/main/java/com/primalsoup/topo/brasov_route_topo/Controller/NavigationController.java
+++ src/main/java/com/primalsoup/topo/brasov_route_topo/Controller/NavigationController.java
@@ -1,8 +1,14 @@
 package com.primalsoup.topo.brasov_route_topo.Controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class NavigationController {
@@ -12,5 +18,18 @@ public class NavigationController {
 		model.addAttribute("body", "page-home");
 
 		return "main-layout";
+	}
+	
+	@GetMapping("/login")
+	public String getLoginPage(Model model) {
+		model.addAttribute("body", "page-login");
+
+		return "main-layout";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    request.logout();  
+	    return "redirect:/";
 	}
 }
