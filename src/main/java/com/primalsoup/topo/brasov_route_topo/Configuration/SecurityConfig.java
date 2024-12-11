f7b16b4398c9ef1278d9b8bae6bf8384ba099068
Cosmin-Valentin <cosminv.nicola@gmail.com>
2024-12-11 09:35:34 +0200

    Spring Security with Thymeleaf for navigation links

--- src/main/java/com/primalsoup/topo/brasov_route_topo/Configuration/SecurityConfig.java
+++ src/main/java/com/primalsoup/topo/brasov_route_topo/Configuration/SecurityConfig.java
@@ -6,35 +6,33 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 @Configuration
 public class SecurityConfig {
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-    	http
-        .securityMatcher("/**")
-        .authorizeHttpRequests(authorize -> {
-            authorize
-                .requestMatchers("/", "/login", "/logout").permitAll()  
-                .requestMatchers("/add-route").authenticated() 
-                .anyRequest().permitAll(); 
-        })
-        .formLogin(form -> {
-            form
-                .loginPage("/login") 
-                .defaultSuccessUrl("/", true) 
-                .failureUrl("/login?error=true") 
-                .permitAll(); 
-        })
-        .logout(logout -> logout
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/")
-            .permitAll()
-        );
+        http
+            .securityMatcher("/**")
+            .authorizeHttpRequests(authorize -> {
+                authorize
+                    .requestMatchers("/", "/login", "/logout").permitAll()  
+                    .requestMatchers("/add-route").authenticated() 
+                    .anyRequest().permitAll(); 
+            })
+            .formLogin(form -> {
+                form
+                    .loginPage("/login") 
+                    .defaultSuccessUrl("/", true) 
+                    .failureUrl("/login?error=true") 
+                    .permitAll(); 
+            })
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+            );
 
         return http.build();
     }
@@ -42,5 +40,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
     }
 }
