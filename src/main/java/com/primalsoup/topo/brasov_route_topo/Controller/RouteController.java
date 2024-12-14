0496b9e2ffaa81a27e73b57c493c39d9009282bc
Cosmin-Valentin <cosminv.nicola@gmail.com>
2024-12-14 17:43:40 +0200

    Route deletion without redirect

--- src/main/java/com/primalsoup/topo/brasov_route_topo/Controller/RouteController.java
+++ src/main/java/com/primalsoup/topo/brasov_route_topo/Controller/RouteController.java
@@ -9,6 +9,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -127,9 +130,14 @@ public class RouteController {
 	}
 	
 	@PostMapping("/remove-route")
-	public String removeRoute(Long id) {
-	    routeService.deleteRouteById(id);
-	    return "redirect:/routes";
+	@ResponseBody
+	public ResponseEntity<String> removeRoute(@RequestParam Long id) {
+		try {
+	        routeService.deleteRouteById(id);
+	        return ResponseEntity.ok().build();
+	    } catch (Exception e) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	    }
 	}
 
 	private void populateModelForForm(Model model, RouteForm routeForm, List<ObjectError> errors) throws JsonProcessingException {
